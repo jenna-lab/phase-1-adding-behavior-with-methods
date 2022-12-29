306087711063da58534241c36e1654f4f14b59e2
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-12-29 18:31:57 +0300

    incomplete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Cat {
+  //...
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    this.speak = function () {
+      return `${this.name} says meow!`;
+    };
+  }
+}
+
+class Dog {
+  //...
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    this.speak = function () {
+      return `${this.name} says woof!`;
+    };
+  }
+}
+
+class Bird {
+  //...
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    this.speak = function () {
+      return `It'\s me! ${this.name},the parrot!`;
+    };
+  }
+}
+// class Bird2 {
+//   //...
+//   constructor(name, sex) {
+//     this.name = name;
+//     this.sex = sex;
+//     this.speak = function () {
+//       return `${this.name} says squawk!`;
+//     };
+//   }
+// }
